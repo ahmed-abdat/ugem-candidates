a6f4b0ff53ed9d47457381349c7e866ab931c1ef
ahmed-abdat <ahmedeabdat@gmail.com>
2025-02-15 01:51:29 +0100

    Modify UserData interface visibility in store configuration

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { userStorage } from "@/lib/storage";
 
-export interface UserData {
+interface UserData {
   id: string;
   first_name: string;
   last_name: string;
